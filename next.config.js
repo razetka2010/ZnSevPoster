a93c8e50d60ac05bcce7969700bdcea9200dfe9d
razetka <128233067+razetka2010@users.noreply.github.com>
2026-05-20 20:10:18 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'picsum.photos' },
-    ],
-    unoptimized: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**': ['./data/**'],
+    },
   },
 };
 
